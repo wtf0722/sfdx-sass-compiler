74eacf5e32d9585b77ca307cd818632266556f7f
Trey Flowers <treytest@commallama.com>
2019-01-22 22:45:47 -0500

    initial commit

---
+++ cli/watch.js
@@ -0,0 +1,20 @@
+const { Command, flags } = require('@oclif/command');
+const { watch } = require('./../');
+
+class WatchCommand extends Command {
+  async run() {
+    const { flags } = this.parse(WatchCommand);
+    await watch({
+      flags,
+      logEnabled: true
+    });
+  }
+}
+
+WatchCommand.description = 'watch for changes';
+
+WatchCommand.flags = {
+  username: flags.string({char: 'u', description: 'a username or alias for the target org'}),
+}
+
+module.exports = WatchCommand;
